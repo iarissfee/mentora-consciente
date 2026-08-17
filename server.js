c71a5a4984cef3c699baa92271d3e408df3df319
iarissfee <iariitta@hotmail.com>
2026-08-17 13:36:19 -0300

    Use PostgreSQL persistence bootstrap

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const { patchSource } = require('./postgres_patch');
 const __partsDir = path.join(__dirname, 'server_chunks');
 const __bundleSource = fs.readdirSync(__partsDir)
   .filter((name) => /^chunk-\d+\.txt$/.test(name))
   .sort()
   .map((name) => fs.readFileSync(path.join(__partsDir, name), 'utf8'))
   .join('');
-eval(__bundleSource);
+eval(patchSource(__bundleSource));
